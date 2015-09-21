76931a24b773b7cbc859d20d4a5959305ff92574
Anirudh-N <Anirudh.Narasimhamurthy@utah.edu>
2015-09-21 00:11:13 -0600

    Cleaned upcode, first part working fine

--- Programming Assignment 1/ngrams.py
+++ Programming Assignment 1/ngrams.py
@@ -6,8 +6,9 @@ from collections import Counter
 if len(sys.argv)>1:
 	training_file=sys.argv[1]
 	test_file=sys.argv[2]
+	seed_file=sys.argv[3]
 else:
-   	print 'Please provide both the training and test files as arguments to execute this program'
+   	print 'Please provide the training, test and seed files as arguments to execute this program'
    
    
 #Reading the contents of the training file   
@@ -32,7 +33,8 @@ count_hash=len(training_data)
 
 
 ################ CONSTRUCTION OF UNIGRAMS ###############################
-'''Converting all data in the file to lower-case and splitting based on space to store the words Removing the '\n' from each line'''
+
+'''Converting all data in the file to lower-case and splitting based on space to store the words Removing the '\n' from each line and also removing the trailing space using strip()'''
 
 for data in training_data:
 	data1=data.lower().strip();
@@ -40,21 +42,16 @@ for data in training_data:
 	data3=data2.split(" ")
 	unigrams_final_data.append(data3)
 
-#print 'Unigrams list is :', unigrams_final_data[:5]
-
-#Unigrams is a list with all the words in the file split based on ""	
+'''Unigrams is a list with all the words in the file split based on " " '''	
 
 vocabulary_repeated=[]
 unigrams_table={}
 
-#Joining list of lists into a single list to get all words into a single list.Filtering out the '' because the file format was of the form a b c . \n
+'''Joining list of lists into a single list to get all words into a single list.'''
 
 vocabulary_repeated=list(itertools.chain(*unigrams_final_data))
-#vocabulary_repeated = filter(lambda a: a != '', vocabulary_repeated)
 
-#print 'After cleaning up:',vocabulary_repeated[:10]
-
-#Creating dictionary of (word,count) key value pairs for the entire list or file of words
+'''Creating dictionary of (word,count) key value pairs for the entire list or file of words'''
 
 unigram_vocabulary=Counter(vocabulary_repeated)
 unigrams_total_frequency=sum(unigram_vocabulary.values())
@@ -63,25 +60,24 @@ unigrams_total_frequency=sum(unigram_vocabulary.values())
 
 bigrams_unigrams_vocabulary=Counter(vocabulary_repeated)
 
-#Adding the (hash,count) value to the dictionary
+''' Adding the (hash,count) value to the dictionary so that it could be used for bigrams probability calculation'''
 
 bigrams_unigrams_vocabulary['#']=count_hash
         
 #print 'Vocabulary is :',bigrams_vocabulary   
 #print 'Number of times # occurs is :', bigrams_unigrams_vocabulary['#']     
-#print 'Number of times i occurs is :', bigrams_unigrams_vocabulary['i']  
-
+ 
 
 ################ BIGRAM PROCESSING ########################################
 
 
-# List of lists where each list contains the content of each line
+''' List of lists where each list contains the content of each line '''
 for data in training_data:
 	data1=data.lower().strip();
 	data2=data1.replace("\n","")
 	bigrams_final_data.append(data2)
 
-
+'''Looping through each line and then adding all the bigrams to a universal list '''
 for data in bigrams_final_data:
 	data1=data.split(" ");
 	data1.insert(0,"#")
@@ -93,7 +89,6 @@ for data in bigrams_final_data:
 bigrams_vocabulary=Counter(bigrams_source_data)
 
 #print bigrams_vocabulary
-
 #print bigrams_vocabulary[('#', 'i')]
 
         
@@ -103,9 +98,10 @@ bigrams_vocabulary=Counter(bigrams_source_data)
 def unigrams_prob(sentence):
 
 	prob_value=0
+	''' Splitting the given sentence based on space to get the words and filtering/removing the trailing spaces in the sentence or line given to us '''
 	data_array=sentence.split(" ")
 	data_array = filter(lambda a: a != '', data_array)
-	#print data_array
+	
 	for i in range(0, len(data_array)):
 	
 		#print 'word is:',data_array[i]
@@ -130,10 +126,15 @@ def unigrams_prob(sentence):
 ##################### BIGRAM PROBABILITY FUNCTION ##########################
 
 def bigrams_prob(sentence):
+
 	prob_value=0
+	''' Splitting the given sentence based on space to get the words and filtering/removing the trailing spaces in the sentence or line given to us '''
+	
 	data_array=sentence.split(" ")
 	data_array=filter(lambda a:a!='',data_array)
-			
+		
+	#Inserting the start of sentence indicator as #	
+		
 	data_array.insert(0,'#')
 	
 	for i in range(0, len(data_array)-1):
@@ -145,9 +146,6 @@ def bigrams_prob(sentence):
 		
 		a=bigrams_vocabulary[(data_array[i],data_array[i+1])]
 		b=bigrams_unigrams_vocabulary[data_array[i]]
-	
-		#print 'a is :', a
-		#print 'b is :', b
 	
 		if  a == 0 :
 			return 'undefined'
@@ -166,10 +164,7 @@ def smoothed_bigrams_prob(sentence):
 	data_array=sentence.split(" ")
 	data_array=filter(lambda a:a!='',data_array)
 	
-	
-	#print data_array
-	'''if(len(data_array) < 2):
-		return 'undefined' '''
+	#Inserting the start of sentence indicator as #	
 		
 	data_array.insert(0,'#')
 	
@@ -180,6 +175,8 @@ def smoothed_bigrams_prob(sentence):
 		#print 'Freq count of word:', bigrams_vocabulary[(data_array[i],data_array[i+1])]
 		#print 'Total frequency count:',unigram_vocabulary[data_array[i]]
 		
+		'''Since V is the vocabulary size which refers to the number of unique terms, this count should not include the '#' which we added for computing the bigram probabilities'''
+		
 		V=len(unigram_vocabulary.keys())
 		a=bigrams_vocabulary[(data_array[i],data_array[i+1])] + 1
 		b=bigrams_unigrams_vocabulary[data_array[i]] + V	
@@ -187,9 +184,6 @@ def smoothed_bigrams_prob(sentence):
 		prob_value += math.log(a/float(b),2)
 
 	return round(prob_value,4)
-
-
-
 
 
 
@@ -218,4 +212,5 @@ for i in range(0, len(cleaned_data)):
   print 'Smoothed Bigrams:logprob(S) =', smoothed_bigrams_prob(cleaned_data[i])	
 	
 	
+
 
