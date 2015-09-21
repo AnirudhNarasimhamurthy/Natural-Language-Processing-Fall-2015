7e2d286cf6c677b3f296777fd8635185161cab95
Anirudh-N <Anirudh.Narasimhamurthy@utah.edu>
2015-09-20 23:03:51 -0600

    Finished bigrams

--- Programming Assignment 1/ngrams.py
+++ Programming Assignment 1/ngrams.py
@@ -19,7 +19,10 @@ with open(training_file, 'r') as inputFile:
 #################### TRAINING DATA PROCESSING #########################
 
 unigrams_final_data=[]
-bigrams=[]	   
+bigrams_final_data=[]
+bigrams_source_data=[]
+bigrams_list=[]	
+bigrams=()   
    
 '''Counting the number of hashes as the number of lines since we assume every sentence would start with a hash'''
 
@@ -32,7 +35,7 @@ count_hash=len(training_data)
 '''Converting all data in the file to lower-case and splitting based on space to store the words Removing the '\n' from each line'''
 
 for data in training_data:
-	data1=data.lower();
+	data1=data.lower().strip();
 	data2=data1.replace("\n","")
 	data3=data2.split(" ")
 	unigrams_final_data.append(data3)
@@ -47,7 +50,7 @@ unigrams_table={}
 #Joining list of lists into a single list to get all words into a single list.Filtering out the '' because the file format was of the form a b c . \n
 
 vocabulary_repeated=list(itertools.chain(*unigrams_final_data))
-vocabulary_repeated = filter(lambda a: a != '', vocabulary_repeated)
+#vocabulary_repeated = filter(lambda a: a != '', vocabulary_repeated)
 
 #print 'After cleaning up:',vocabulary_repeated[:10]
 
@@ -56,17 +59,43 @@ vocabulary_repeated = filter(lambda a: a != '', vocabulary_repeated)
 unigram_vocabulary=Counter(vocabulary_repeated)
 unigrams_total_frequency=sum(unigram_vocabulary.values())
 
-print 'Total unigram frequency:', unigrams_total_frequency
+#print 'Total unigram frequency:', unigrams_total_frequency
 
-bigrams_vocabulary=Counter(vocabulary_repeated)
+bigrams_unigrams_vocabulary=Counter(vocabulary_repeated)
 
 #Adding the (hash,count) value to the dictionary
 
-bigrams_vocabulary['#']=count_hash
+bigrams_unigrams_vocabulary['#']=count_hash
         
 #print 'Vocabulary is :',bigrams_vocabulary   
+print 'Number of times # occurs is :', bigrams_unigrams_vocabulary['#']     
+print 'Number of times i occurs is :', bigrams_unigrams_vocabulary['i']  
 
-print 'Number of times banjo occurs is :', unigram_vocabulary['banjo']     
+
+################ BIGRAM PROCESSING ########################################
+
+
+# List of lists where each list contains the content of each line
+for data in training_data:
+	data1=data.lower().strip();
+	data2=data1.replace("\n","")
+	bigrams_final_data.append(data2)
+
+
+for data in bigrams_final_data:
+	data1=data.split(" ");
+	data1.insert(0,"#")
+	for i in range(0, len(data1)-1):
+		bigrams=(data1[i],data1[i+1])
+		bigrams_source_data.append(bigrams);
+
+
+bigrams_vocabulary=Counter(bigrams_source_data)
+
+#print bigrams_vocabulary
+
+print bigrams_vocabulary[('#', 'i')]
+
         
 
 ################ UNIGRAM PROBABILITY FUNCTION ##########################
@@ -96,6 +125,44 @@ def unigrams_prob(sentence):
 
 
 
+
+
+##################### BIGRAM PROBABILITY FUNCTION ##########################
+
+def bigrams_prob(sentence):
+	prob_value=0
+	data_array=sentence.split(" ")
+	data_array=filter(lambda a:a!='',data_array)
+	
+	
+	#print data_array
+	if(len(data_array) < 2):
+		return 'undefined'
+		
+	data_array.insert(0,'#')
+	
+	for i in range(0, len(data_array)-1):
+	
+		
+		print 'word is:',(data_array[i],data_array[i+1])
+		#print 'Freq count of word:', bigrams_vocabulary[(data_array[i],data_array[i+1])]
+		#print 'Total frequency count:',unigram_vocabulary[data_array[i]]
+		
+		a=bigrams_vocabulary[(data_array[i],data_array[i+1])]
+		b=bigrams_unigrams_vocabulary[data_array[i]]
+	
+		print 'a is :', a
+		print 'b is :', b
+	
+		if  a == 0 :
+			return 'undefined'
+			break;
+		else:
+			prob_value += math.log(a/float(b),2)
+
+	return prob_value
+
+
 ###################### TEST DATA PROCESSING ###############################
 
 #Variable declarations
@@ -117,8 +184,8 @@ for data in test_data:
 for i in range(0, len(cleaned_data)):
   print 'S = ',cleaned_data[i]
   print 'Unigrams:logprob(S) = %.4f' %unigrams_prob(cleaned_data[i])
-  '''print 'Bigrams:logprob(S) =', bigrams_prob(cleaned_data[i])
-  print 'Smoothed Bigrams:logprob(S) =', smoothed_bigrams_prob(cleaned_data[i])'''	
+  print 'Bigrams:logprob(S) =', bigrams_prob(cleaned_data[i])
+  #print 'Smoothed Bigrams:logprob(S) =', smoothed_bigrams_prob(cleaned_data[i])	
 	
 	
 
