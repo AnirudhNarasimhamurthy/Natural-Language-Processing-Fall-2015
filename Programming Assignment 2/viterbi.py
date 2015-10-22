50889ebe0aed8083738acbc5a1cbd18fb56ee890
Anirudh-N <Anirudh.Narasimhamurthy@utah.edu>
2015-10-21 21:31:08 -0600

    Initialization step completed

--- Programming Assignment 2/viterbi.py
+++ Programming Assignment 2/viterbi.py
@@ -14,13 +14,16 @@ else:
    	print 'Please provide the probabilities file and sentences file as inputs to the program in the specified order <probabilities_file>, <sentences_file>'
      
 
-# Variables declaration   
+########## Variables declaration   #################
 
 words_sent=[] #List of lists to store each sentence's words
 W=0  #Number of words in the sentence
-T=5 # In this case we have only four POS tags + phi symbol
+pos_tags=['noun','verb','inf','prep']
+T=len(pos_tags) # In this case we have only four POS tags + phi symbol
 prob_data_list=[]
 prob_dict={}
+
+
    
 #Reading the contents of the probabilities file   
 
@@ -35,12 +38,6 @@ print 'Probabilities values are :', prob_data_list
 
 # Storing the values from the probabilities file as a dictionary of key,value pairs where key is POS bigram /lexical generation pairs
 
-#Creating a list of lists first
-
-'''for i in range(0, len(prob_data)):
-	prob_data_list.append(prob_data[i].split(" "))'''
-
-#Creating the dictionary of key,value pairs here
 for i in range(0, len(prob_data_list)):
 	key=(prob_data_list[i][0],prob_data_list[i][1])
 	value=float(prob_data_list[i][2])
@@ -49,15 +46,50 @@ for i in range(0, len(prob_data_list)):
 print 'The prob dict values are :', prob_dict	
 	
 
+
+############################ VITERBI ALGORITHM ################################
+
+
+def viterbi(sent, W, T):
+
+	score=[[0 for x in range(W)]  for i in range(T)]
+	#print 'Score is :', score
+	#print 'No of tags is :', T
+	
+	############   Initialization step   ################
+	for t in range(0, T):
+		word1_tagt=(sent[0],pos_tags[t])
+		print 'Word1 | tag_t is :', word1_tagt
+		tagt_phi=(pos_tags[t],'phi')
+		print 'Tag_t | phi is :', tagt_phi
+		
+		# Four possible cases handled below
+		
+		if word1_tagt in prob_dict:
+			if tagt_phi in prob_dict:
+				score[t][0]=prob_dict[word1_tagt] * prob_dict[tagt_phi] #Both are in dict
+			else:
+				score[t][0]=prob_dict[word1_tagt] * 0.001   #tagt_phi is not in dict
+		else:
+			if tagt_phi in prob_dict:
+				score[t][0]= 0.0001 * prob_dict[tagt_phi]	#word1_tagt is not in dict
+			else:
+				score[t][0]= 0.0001 * 0.0001	            # both are not in dict
+
+		print 'P(' + sent[0] + '=' + pos_tags[t] +') = %.10f' %score[t][0]
+
+
+############################# SENTENCE FILE PROCESSING #################################
+
 # Reading the contents of the sentences file
 
 with open(sentences_file, 'r') as inputFile:
 	sent_data = inputFile.readlines()
 
 for i in range(0, len(sent_data)):
-	sent_data[i]=sent_data[i].replace('\n', '')
+	sent_data[i]=sent_data[i].replace('\n', '').lower()
 	
-#print 'Sentences values are :', sent_data
+print 'Sentences values are :', sent_data
 
 
 # Storing the words in each sentence in a list of lists
@@ -71,3 +103,4 @@ for i in range(0, len(words_sent)):
 	print 'PROCESSING SENTENCE:', ' '.join(words_sent[i])
 	W=len(words_sent[i])
 	print 'No of words in the sentence is :', W
+	viterbi(words_sent[i],W,T)
