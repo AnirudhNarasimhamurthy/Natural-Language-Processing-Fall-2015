83cb23487f03b98e163042c1e4a97c879b47cfd7
Anirudh-N <Anirudh.Narasimhamurthy@utah.edu>
2015-11-08 23:33:08 -0700

    Stripped out questionID and questions from every question file into a list

--- Project/qa.py
+++ Project/qa.py
@@ -2,6 +2,8 @@ __author__ = 'Anirudh'
 
 
 import sys
+import QP
+
 
 ################ Getting the input file from the command line arguments ###########################
 
@@ -18,13 +20,13 @@ with open(input_file, 'r') as input:
 for i in range(0, len(data)):
     data[i]=data[i].replace("\n","")
 
-print 'Data is :', data
+#print 'Data is :', data
 
 
 ######################## Getting the question and story from the given input story ID ##############################
 
 directory_path=data[0]
-print 'Directory path is :', directory_path
+#print 'Directory path is :', directory_path
 
 #######################  Computing the full directory path from the storyID for story and question  #######################
 
@@ -32,9 +34,28 @@ for i in range(1, len(data)):
     story_id=data[i]
     question_path=directory_path + '/' + story_id + '.questions'
     story_path=directory_path + '/' + story_id + '.story'
-    print 'Question file is :', question_path
-    print 'Story file is :', story_path
+    #print 'Question file is :', question_path
+    #print 'Story file is :', story_path
 
-    ################## Reading the corresponding story and the questions for the given story id ###################
+    ################## Reading the corresponding story file for the given story id ###################
     with open(story_path, 'r') as storyFile:
         story=storyFile.readlines()
+
+    for i in range(0,len(story)):
+        story[i]=story[i].replace("\n","")
+
+    #print 'Story is :', story
+
+    ################## Reading the corresponding question file for the given story id ###################
+    with open(question_path, 'r') as questionFile:
+        question=questionFile.readlines()
+
+    for i in range(0,len(question)):
+        question[i]=question[i].replace("\n","")
+
+    #print 'Question is :', question
+
+    qIDList, qList = QP.question_parser(question)
+
+    print 'QuestionID List is :', qIDList
+    print 'Question list is :', qList
